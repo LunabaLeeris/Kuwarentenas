8559498e0f89d5a7683e58c479f2471ecb5c8aad
Christian Lee Lunaba <lunabachristianlee019@gmail.com>
2024-04-18 19:59:07 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,13 +2,7 @@ import { getDatabase, ref, get} from "firebase/database"
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAI4XCCU8S2qwzniXb3K8v3zJts8ieKg9Q",
-  authDomain: "kuwarentenas.firebaseapp.com",
-  databaseURL: "https://kuwarentenas-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "kuwarentenas",
-  storageBucket: "kuwarentenas.appspot.com",
-  messagingSenderId: "180140898789",
-  appId: "1:180140898789:web:e2cdcfa585edd3fb47170c"
+  // Input your database here
 };
 
 // Initialize Firebase
